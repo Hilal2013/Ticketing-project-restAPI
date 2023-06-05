02ad298c8e5486bedbdb64aa3c68268b55387a2e
Hilal2013 <hilalozkul@gmail.com>
2023-06-05 12:54:49 -0400

    updated

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 @RequestMapping("/api/v1/project")
 public class ProjectController {
@@ -19,6 +21,7 @@ public class ProjectController {
 
 
     @GetMapping
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> getProjects() {
         return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",
                 projectService.listAllProjects(), HttpStatus.OK));
@@ -27,12 +30,14 @@ public class ProjectController {
     }
 
     @GetMapping("/{code}")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> getProjectByCode(@PathVariable("code") String code) {
         return ResponseEntity.ok(new ResponseWrapper("Project is successfully retrieved",
                 projectService.getByProjectCode(code), HttpStatus.OK));
     }
 
     @PostMapping
+    @RolesAllowed({"Admin", "Manager"})
     public ResponseEntity<ResponseWrapper> createProject(@RequestBody ProjectDTO project) {//Im capturing that project from the API
         projectService.save(project);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Project is  successfully created", HttpStatus.CREATED));
@@ -40,22 +45,29 @@ public class ProjectController {
     }
 
     @PutMapping
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> updateProject(@RequestBody ProjectDTO project) {
         projectService.update(project);
         return ResponseEntity.ok(new ResponseWrapper("Project is  successfully updated", HttpStatus.OK));
 
     }
-@DeleteMapping("/{projectCode}")
+
+    @DeleteMapping("/{projectCode}")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> deleteProject(@PathVariable("projectCode") String projectCode) {
-    projectService.delete(projectCode);
-    return ResponseEntity.ok(new ResponseWrapper("Project is  successfully deleted", HttpStatus.OK));
+        projectService.delete(projectCode);
+        return ResponseEntity.ok(new ResponseWrapper("Project is  successfully deleted", HttpStatus.OK));
     }
-@GetMapping("/manager/project-status") //is gonna show the project assigned to manager
+
+    @GetMapping("/manager/project-status") //is gonna show the project assigned to manager
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> getProjectByManager() {
-    return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",
-            projectService.listAllProjectDetails(), HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",
+                projectService.listAllProjectDetails(), HttpStatus.OK));
     }
+
     @PutMapping("/manager/complete/{projectCode}")//you are trying change field from open to complete
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> managerCompleteProject(@PathVariable("projectCode") String projectCode) {
         projectService.complete(projectCode);
         return ResponseEntity.ok(new ResponseWrapper("Project is successfully completed", HttpStatus.OK));
