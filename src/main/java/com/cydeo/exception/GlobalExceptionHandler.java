6b6f772e92e9bd34d08fe2229254cd5900408ff0
Hilal2013 <hilalozkul@gmail.com>
2023-06-06 12:10:11 -0400

    updated

--- src/main/java/com/cydeo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cydeo/exception/GlobalExceptionHandler.java
@@ -15,21 +15,21 @@ import org.springframework.web.method.HandlerMethod;
 import java.lang.reflect.Method;
 import java.util.Optional;
 
-@RestControllerAdvice
+@RestControllerAdvice //AOP//whenver exeption happens this class will execute
 public class GlobalExceptionHandler {
-
+//whenever exception happens  belongs to my own custom class execute this code//im modifying json output
     @ExceptionHandler(TicketingProjectException.class)
     public ResponseEntity<ResponseWrapper> serviceException(TicketingProjectException se){
         String message = se.getMessage();
         return new ResponseEntity<>(ResponseWrapper.builder().success(false).code(HttpStatus.CONFLICT.value()).message(message).build(),HttpStatus.CONFLICT);
     }
-
+//if any exception happens belongs to accesDeniedClass show this one//springBoot automotically throws
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ResponseWrapper> accessDeniedException(AccessDeniedException se){
         String message = se.getMessage();
         return new ResponseEntity<>(ResponseWrapper.builder().success(false).code(HttpStatus.FORBIDDEN.value()).message(message).build(),HttpStatus.FORBIDDEN);
     }
-
+//if any exception happens belongs to these classes this method will execute
     @ExceptionHandler({Exception    .class, RuntimeException.class, Throwable.class, BadCredentialsException.class})
     public ResponseEntity<ResponseWrapper> genericException(Throwable e, HandlerMethod handlerMethod) {
 
@@ -43,9 +43,10 @@ public class GlobalExceptionHandler {
                     .build();
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+        //if there is no annotation on method execute this one
         return new ResponseEntity<>(ResponseWrapper.builder().success(false).message("Action failed: An error occurred!").code(HttpStatus.INTERNAL_SERVER_ERROR.value()).build(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
+//
 
     private Optional<DefaultExceptionMessageDto> getMessageFromAnnotation(Method method) {
         DefaultExceptionMessage defaultExceptionMessage = method.getAnnotation(DefaultExceptionMessage.class);
